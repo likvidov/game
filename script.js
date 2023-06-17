ae60c9d2eb2ac2c111f156bcfd53ec0443cf051b
likvidov <likvidya@gmail.comm>
2023-06-17 18:32:53 +0300

    Выполненое сложное задание

--- script.js
+++ script.js
@@ -8,22 +8,29 @@ let getRandomNumber = function () {
   return 1 + Math.floor(Math.random() * 100)
 };
 
-const game = function (randomNumber) {
+const game = function (randomNumber, counterTry = 10) {
   let number = prompt('Угадай число от 1 до 100');
+  --counterTry;
 
   if (number === null) {
-    alert("Игра окончена");;
+    alert("Игра окончена");
   } else if (!isNumber(number)) {
     alert('Введите число');
     game(randomNumber);
-  } else if (number > randomNumber) {
-    alert('Загаданное число меньше');
-    game(randomNumber);
-  } else if (number < randomNumber) {
-    alert('Загаданное число больше');
-    game(randomNumber);
+  } else if (number > randomNumber && counterTry > 0) {
+    alert('Загаданное число меньше, осталось попыток ' + counterTry);
+    game(randomNumber, counterTry);
+  } else if (number < randomNumber && counterTry > 0) {
+    alert('Загаданное число больше, осталось попыток ' + counterTry);
+    game(randomNumber, counterTry);
   } else if (number == randomNumber) {
-    alert('Поздравляю, Вы угадали!!!');
+    if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
+      game(getRandomNumber(), 10)
+    }
+  } else {
+    if (confirm('Попытки закончились, хотите сыграть еще?')) {
+      game(getRandomNumber(), 10)
+    }
   }
 }
 
