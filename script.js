6ea91f3c53b134141a60cc6ca66893355247139f
likvidov <likvidya@gmail.comm>
2023-06-21 11:02:53 +0300

    Выполнил исправление

--- script.js
+++ script.js
@@ -8,28 +8,40 @@ let getRandomNumber = function () {
   return 1 + Math.floor(Math.random() * 100)
 };
 
-const game = function (randomNumber, counterTry = 10) {
+const game = function (randomNumber, counterTry = 3) {
   let number = prompt('Угадай число от 1 до 100');
-  --counterTry;
+  console.log(randomNumber);
+  let count = counterTry;
 
-  if (number === null) {
-    alert("Игра окончена");
-  } else if (!isNumber(number)) {
+  if (number == null) {
+    return alert("Игра окончена");
+  }
+
+  if (!isNumber(number)) {
     alert('Введите число');
-    game(randomNumber);
-  } else if (number > randomNumber && counterTry > 0) {
-    alert('Загаданное число меньше, осталось попыток ' + counterTry);
-    game(randomNumber, counterTry);
-  } else if (number < randomNumber && counterTry > 0) {
-    alert('Загаданное число больше, осталось попыток ' + counterTry);
-    game(randomNumber, counterTry);
-  } else if (number == randomNumber) {
+    game(randomNumber, count);
+  }
+
+  --count;
+
+  if (number > randomNumber && count > 0) {
+    alert('Загаданное число меньше, осталось попыток ' + count);
+    game(randomNumber, count);
+  }
+  if (number < randomNumber && count > 0) {
+    alert('Загаданное число больше, осталось попыток ' + count);
+    game(randomNumber, count);
+  }
+
+  if (number == randomNumber && count >= 0) {
     if (confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')) {
-      game(getRandomNumber(), 10)
+      game(getRandomNumber())
     }
-  } else {
+  }
+
+  if (count <= 0) {
     if (confirm('Попытки закончились, хотите сыграть еще?')) {
-      game(getRandomNumber(), 10)
+      game(getRandomNumber())
     }
   }
 }
